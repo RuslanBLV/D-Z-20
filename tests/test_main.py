0f3efd7aa437ee495187625c13c67faabfea37d8
Ruslan <rus.beluaev.101@gmail.com>
2025-04-12 18:30:23 +0700

    2

--- tests/test_main.py
+++ tests/test_main.py
@@ -209,3 +209,17 @@ def test_mixin_log(capsys):
     Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     messange = capsys.readouterr()
     assert messange.out.strip() == "Product('Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5')"
+
+
+def test_error():
+    with pytest.raises(ValueError):
+        Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+
+
+def test_middle_price():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
+    assert category1.middle_price() == 140333.33
